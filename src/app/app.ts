fd803143196c435bec136e75ee5eddfe55d4f534
Alexia8800 <alessiaagiuva@gmail.com>
2026-05-09 10:55:54 +0200

    modifiche footer e navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
